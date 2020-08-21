3ba686df238fb64bda16129052313fd3b5599d6b
Brian-Weloba <bweloba@gmail.com>
2020-08-21 23:41:30 +0300

    update function to get day of week to .js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,24 @@ var dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturd
 var akanNameMale = ["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Koffi", "Kwame", "Kwasi"];
 var akanNameFemale = ["Adwoa", "Abena", "Akua", "Yaa", "Aufa", "Amma", "Akosua"];
 
+
+
 var dayOfWeek = function() {
     var year = document.getElementById("year").value;
     CC = parseInt(year.substring(0, 2));
     YY = parseInt(year.substring(2, 4));
     MM = parseInt(document.getElementById("month").value);
     DD = parseInt(document.getElementById("dob").value);
-    var day = (DD + MM + YY + CC) % 7;
-    console.log(year, CC, YY, MM, DD);
-    return (Math.floor(day));
+    if (MM === 1) {
+        MM = 11;
+        YY--;
+    } else if (MM === 2) {
+        MM = 12;
+        YY--;
+    } else {
+        MM -= 2;
+    }
+    console.log(CC, YY, MM, DD);
+    var day = DD + ((13 * MM - 1) / 5) + YY + (YY / 4) + (CC / 4) - 2 * CC;
+    return Math.floor(day % 7);
 };
